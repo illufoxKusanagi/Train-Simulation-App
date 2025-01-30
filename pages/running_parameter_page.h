3cc7cc7e4dfba805a817255a89ee0679a7221e2f
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-01-31 06:04:40 +0700

    style: adding running parameter page

--- pages/running_parameter_page.h
+++ pages/running_parameter_page.h
@@ -1,4 +1,19 @@
 #ifndef RUNNINGPARAMETERWIDGET_H
 #define RUNNINGPARAMETERWIDGET_H
 
+#include "../widgets/button_action.h"
+#include "../widgets/input_widget.h"
+#include <QGridLayout>
+#include <QWidget>
+
+
+class RunningParameterWidget : public QWidget {
+  Q_OBJECT
+public:
+  explicit RunningParameterWidget(QWidget *parent = nullptr);
+
+private:
+  QGridLayout *formLayout;
+};
+
 #endif // RUNNINGPARAMETERWIDGET_H
