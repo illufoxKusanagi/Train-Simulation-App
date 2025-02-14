d4cf0dd28c45492c09406ec8b55cd6ccaf2b719c
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-02-14 13:29:58 +0700

    feat: remove unecessary codes and fix saving acceleration bug

--- formulas/traction_motor.cpp
+++ formulas/traction_motor.cpp
@@ -1,10 +1,25 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
 
-float f_tractionMotor;
-float t_motor;
-float rpm_motor;
+double tm_f;
+double tm_t;
+double tm_rpm;
+
+double f_motor;
+float v;
+
+int n_train;
+float gearRatio;
 int wheel;
-double gearRatio;
+
+void calculateTractionForce() { tm_f = f_motor / n_train; }
+
+void calculateTorque() { tm_t = (f_motor * (wheel / (2 * 100))) / gearRatio; }
+
+void calculateRpm() {
+  tm_rpm = ((v * 1000 / 60) * gearRatio) / ((M_PI * wheel) / 1000);
+}
 
 int main() {
   std::cout << "Hello World!" << std::endl;
