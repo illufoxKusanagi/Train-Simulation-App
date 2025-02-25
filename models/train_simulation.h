3321436c23315355cc7108ab65e591ab78b96470
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-02-25 11:00:27 +0700

    feat: adding ass functions in train_simulation.cpp file

--- models/train_simulation.h
+++ models/train_simulation.h
@@ -1,10 +1,52 @@
 #ifndef TRAIN_SIMULATION_H
 #define TRAIN_SIMULATION_H
 
-class TrainSimulation
-{
-public:
-    TrainSimulation();
-};
+#define _USE_MATH_DEFINES
+#include "train_data.h"
+#include <QString>
+#include <cmath>
+
+ConstantData constantData;
+TrainData trainData;
+MassData massData;
+LoadData loadData;
+ResistanceData resistanceData;
+MovingData movingData;
+TrainMotorData trainMotorData;
+EfficiencyData efficiencyData;
+PowerData powerData;
+EnergyData energyData;
+
+double countMassEmptyCar();
+double countMassWithLoad();
+double countMassLoadInput();
+double countInertialMass();
+double countInertialMassInput();
+double calculateResTrain(float m, float startRes);
+double calculateResSlope(float m, float slope);
+double calculateResRadius(float m, float radius);
+double calculateStartForce();
+double calculateTotalForce(float v);
+double calculateBrakeForce();
+double calculateTotalBrakeForce();
+double calculateTractionForce();
+double calculateTorque();
+double calculateRpm();
+double calculateAdhesion();
+double calculateResistanceForcePerMotor(double f_res);
+double calculatePowerWheel();
+double calculateOutputTractionMotor();
+double calculateInputTractionMotor();
+double calculatePowerInputOfVvvf();
+double calculatePowerOfCatenary();
+void calculatePoweringForce(float acc, float v);
+void calculateBrakingForce();
+void simulateStaticTrainMovement();
+void simulateDynamicTrainMovement();
+
+double calculateEnergyConsumption();
+double calculateEnergyOfPowering();
+double calculateEnergyRegeneration();
+double calculateEnergyOfAps();
 
 #endif // TRAIN_SIMULATION_H
