824d0cf0c8fd324cf3fa176085b94a28e52dd801
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-03-07 14:05:02 +0700

    chore: merge from develop

--- pages/train_power_page.cpp
+++ pages/train_power_page.cpp
@@ -85,46 +85,34 @@ void TrainPowerPage::setupInputs(QVBoxLayout *pageLayout,
 }
 
 void TrainPowerPage::setParameterValue() {
-  // First check if each key exists before accessing
   QList<QString> keys = m_inputWidgets.keys();
 
-  // Reset all values first
   for (const QString &key : keys) {
     if (m_inputWidgets[key]) {
       m_inputWidgets[key]->setValue(0);
     }
   }
-
-  // Set new values only for widgets that exist
   if (m_inputWidgets.contains("Dynamic Catenary Power"))
     m_inputWidgets["Dynamic Catenary Power"]->setValue(
         m_trainSimulation->findMaxCatenaryPower());
-
   if (m_inputWidgets.contains("Dynamic Max VVVF Power"))
     m_inputWidgets["Dynamic Max VVVF Power"]->setValue(
         m_trainSimulation->findMaxVvvfPower());
-
   if (m_inputWidgets.contains("Dynamic Catenary Current"))
     m_inputWidgets["Dynamic Catenary Current"]->setValue(
         m_trainSimulation->findMaxCatenaryCurrent());
-
   if (m_inputWidgets.contains("Dynamic VVVF Current"))
     m_inputWidgets["Dynamic VVVF Current"]->setValue(
         m_trainSimulation->findMaxVvvfCurrent());
-
-  // Static values
   if (m_inputWidgets.contains("Static Catenary Power"))
     m_inputWidgets["Static Catenary Power"]->setValue(
         m_trainSimulation->findMaxCatenaryPower());
-
   if (m_inputWidgets.contains("Static VVVF Power"))
     m_inputWidgets["Static VVVF Power"]->setValue(
         m_trainSimulation->findMaxVvvfPower());
-
   if (m_inputWidgets.contains("Static Catenary Current"))
     m_inputWidgets["Static Catenary Current"]->setValue(
         m_trainSimulation->findMaxCatenaryCurrent());
-
   if (m_inputWidgets.contains("Static VVVF Current"))
     m_inputWidgets["Static VVVF Current"]->setValue(
         m_trainSimulation->findMaxVvvfCurrent());
@@ -136,19 +124,16 @@ void TrainPowerPage::setupPagination() {
   paginationLayout->setAlignment(Qt::AlignLeft);
   paginationLayout->setSpacing(16);
 
-  // Create direct page buttons
   m_firstPageButton = new ButtonAction("Dynamic Power", "normal", this);
   m_secondPageButton = new ButtonAction("Dynamic Current", "normal", this);
   m_thirdPageButton = new ButtonAction("Static Power", "normal", this);
   m_fourthPageButton = new ButtonAction("Static Current", "normal", this);
 
-  // Set fixed size for all buttons
   m_firstPageButton->setFixedSize(144, 48);
   m_secondPageButton->setFixedSize(144, 48);
   m_thirdPageButton->setFixedSize(144, 48);
   m_fourthPageButton->setFixedSize(144, 48);
 
-  // Connect buttons to changePage with their respective page indices
   connect(m_firstPageButton, &QPushButton::clicked, this,
           [this]() { pageChanged(0); });
   connect(m_secondPageButton, &QPushButton::clicked, this,
@@ -157,41 +142,14 @@ void TrainPowerPage::setupPagination() {
           [this]() { pageChanged(2); });
   connect(m_fourthPageButton, &QPushButton::clicked, this,
           [this]() { pageChanged(3); });
-
-  // Add buttons to layout
   paginationLayout->addWidget(m_firstPageButton);
   paginationLayout->addWidget(m_thirdPageButton);
   paginationLayout->addWidget(m_fourthPageButton);
   paginationLayout->addWidget(m_secondPageButton);
 
-  // Only add these if you actually have 3rd and 4th pages
-
   mainLayout->addWidget(paginationWidget);
-
-  // Initialize button states
   updatePageButtons();
 }
-
-// void TrainPowerPage::showPreviousPage() {
-//   if (stackedWidget->currentIndex() > 0) {
-//     stackedWidget->setCurrentIndex(stackedWidget->currentIndex() - 1);
-//     updatePaginationButtons();
-//   }
-// }
-
-// void TrainPowerPage::showNextPage() {
-//   if (stackedWidget->currentIndex() < stackedWidget->count() - 1) {
-//     stackedWidget->setCurrentIndex(stackedWidget->currentIndex() + 1);
-//     updatePaginationButtons();
-//   }
-// }
-
-// void TrainPowerPage::updatePaginationButtons() {
-//   int currentIndex = stackedWidget->currentIndex();
-//   int lastIndex = stackedWidget->count() - 1;
-//   m_prevButton->setEnabled(currentIndex > 0);
-//   m_nextButton->setEnabled(currentIndex < lastIndex);
-// }
 
 void TrainPowerPage::pageChanged(int pageIndex) {
   int maxIndex = stackedWidget->count() - 1;
