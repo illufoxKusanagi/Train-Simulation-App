b296e3e8028ecb69e00574b31735f00bd43c914a
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-04-09 14:24:55 +0700

    resolve conflict

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TrainSimulationApp_FILE F:/matkul/sem_6/AppProject/TrainAppSimulation/build/TrainSimulationApp.exe)
-set(__QT_DEPLOY_TARGET_TrainSimulationApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TrainSimulationApp_RUNTIME_DLLS E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6Concurrent.dll;E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6Charts.dll;E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6OpenGLWidgets.dll;E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6Widgets.dll;E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6OpenGL.dll;E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6Gui.dll;E:/QtCreatorFixed/6.9.0/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TrainAppSimulation_FILE F:/matkul/sem_6/AppProject/TrainAppSimulation/build/Desktop_Qt_6_8_1_MinGW_64_bit-Release/TrainAppSimulation.exe)
-set(__QT_DEPLOY_TARGET_TrainAppSimulation_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TrainAppSimulation_RUNTIME_DLLS E:/QtCreatorFixed/6.8.1/mingw_64/bin/Qt6Charts.dll;E:/QtCreatorFixed/6.8.1/mingw_64/bin/Qt6OpenGLWidgets.dll;E:/QtCreatorFixed/6.8.1/mingw_64/bin/Qt6Widgets.dll;E:/QtCreatorFixed/6.8.1/mingw_64/bin/Qt6OpenGL.dll;E:/QtCreatorFixed/6.8.1/mingw_64/bin/Qt6Gui.dll;E:/QtCreatorFixed/6.8.1/mingw_64/bin/Qt6Core.dll)
