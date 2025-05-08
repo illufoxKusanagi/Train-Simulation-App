e38a9012bce09bb75c06d14c5908ccd9802b2c33
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-05-08 08:47:04 +0700

    fix: fixing bug whenever user drags login form dialog

--- widgets/login_dialog_widget.cpp
+++ widgets/login_dialog_widget.cpp
@@ -2,8 +2,9 @@
 
 LoginDialogWidget::LoginDialogWidget(QObject *parent) {
   QVBoxLayout *layout = new QVBoxLayout(this);
-  layout->setContentsMargins(20, 20, 20, 20);
-  layout->setSpacing(16);
+  layout->setContentsMargins(32, 32, 32, 32);
+  layout->setSpacing(20);
+  layout->setAlignment(Qt::AlignCenter);
 
   QLabel *titleLabel = new QLabel("Train Simulation Login", this);
   titleLabel->setAlignment(Qt::AlignCenter);
@@ -13,27 +14,33 @@ LoginDialogWidget::LoginDialogWidget(QObject *parent) {
                             "}");
   layout->addWidget(titleLabel);
 
+  QVBoxLayout *inputLayout = new QVBoxLayout(this);
+  inputLayout->setContentsMargins(0, 0, 0, 0);
+  inputLayout->setSpacing(8);
+  inputLayout->setAlignment(Qt::AlignCenter);
   m_usernameInput = new InputWidget(this, InputType("field", "Username"));
   m_passwordInput = new InputWidget(this, InputType("field", "Password"));
   m_passwordInput->setEchoMode(QLineEdit::Password);
   m_usernameInput->setPlaceholderText("Enter username...");
   m_passwordInput->setPlaceholderText("Enter password...");
-  m_usernameInput->setSize(300, 32);
-  m_passwordInput->setSize(300, 32);
+  m_usernameInput->setSize(256, 32);
+  m_passwordInput->setSize(256, 32);
 
-  layout->addWidget(m_usernameInput);
-  layout->addWidget(m_passwordInput);
+  inputLayout->addWidget(m_usernameInput);
+  inputLayout->addWidget(m_passwordInput);
 
   m_loginButton = new ButtonAction(this, "Login", "no");
   m_loginButton->setEnabled(true);
-  m_loginButton->setSize(300, 44);
+  m_loginButton->setSize(256, 44);
+  layout->addLayout(inputLayout);
   layout->addWidget(m_loginButton);
   connect(m_loginButton, &QPushButton::clicked, this,
           &LoginDialogWidget::onLoginButtonClicked);
-  setFixedSize(350, 250);
+  setFixedSize(350, 300);
   setWindowTitle("Login");
-  setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::CustomizeWindowHint |
-                 Qt::WindowCloseButtonHint);
+  setModal(true);
+  setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
+  setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 }
 
 void LoginDialogWidget::onLoginButtonClicked() {
