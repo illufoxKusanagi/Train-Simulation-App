47ec2160b75bb56d8c26e25a7788038071f89b82
illufoxKusanagi <ariefsatria2409@gmail.com>
2025-11-27 14:21:52 +0700

    fix: fixing bug at building binaries

--- backend/http_server/inputs/electrical_parameter_handler.h
+++ backend/http_server/inputs/electrical_parameter_handler.h
@@ -1,21 +1,22 @@
 #ifndef ELECTRICAL_PARAMETER_HANDLER_H
 #define ELECTRICAL_PARAMETER_HANDLER_H
 
+#include <QJsonDocument>
 #include <QObject>
+#include <core/appcontext.h>
 #include <qhttpserverresponse.h>
 #include <qjsonobject.h>
-#include <core/appcontext.h>
 
-class ElectricalParameterHandler : public QObject
-{
-    Q_OBJECT
+class ElectricalParameterHandler : public QObject {
+  Q_OBJECT
 public:
-    explicit ElectricalParameterHandler(AppContext &context, QObject *parent = nullptr);
-    QHttpServerResponse handleGetElectricalParameters();
-    QHttpServerResponse handleUpdateElectricalParameters(const QJsonObject &data);
+  explicit ElectricalParameterHandler(AppContext &context,
+                                      QObject *parent = nullptr);
+  QHttpServerResponse handleGetElectricalParameters();
+  QHttpServerResponse handleUpdateElectricalParameters(const QJsonObject &data);
 
 private:
-    AppContext &m_context;
+  AppContext &m_context;
 };
 
 #endif // ELECTRICAL_PARAMETER_HANDLER_H
--- backend/http_server/inputs/running_parameter_handler.h
+++ backend/http_server/inputs/running_parameter_handler.h
@@ -1,21 +1,22 @@
 #ifndef RUNNING_PARAMETER_HANDLER_H
 #define RUNNING_PARAMETER_HANDLER_H
 
+#include <QJsonDocument>
 #include <QObject>
+#include <core/appcontext.h>
 #include <qhttpserverresponse.h>
 #include <qjsonobject.h>
-#include <core/appcontext.h>
 
-class RunningParameterHandler : public QObject
-{
-    Q_OBJECT
+class RunningParameterHandler : public QObject {
+  Q_OBJECT
 public:
-    explicit RunningParameterHandler(AppContext &context, QObject *parent = nullptr);
-    QHttpServerResponse handleGetRunningParameters();
-    QHttpServerResponse handleUpdateRunningParameters(const QJsonObject &data);
+  explicit RunningParameterHandler(AppContext &context,
+                                   QObject *parent = nullptr);
+  QHttpServerResponse handleGetRunningParameters();
+  QHttpServerResponse handleUpdateRunningParameters(const QJsonObject &data);
 
 private:
-    AppContext &m_context;
+  AppContext &m_context;
 };
 
 #endif // RUNNING_PARAMETER_HANDLER_H
